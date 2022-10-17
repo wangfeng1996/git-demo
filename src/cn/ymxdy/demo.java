438745444322929d1a58e5dfbdd6870ea875e6b4
wangfeng <1290715192@qq.com>
2022-10-17 14:29:19 +0800

    git 提交

---
+++ src/cn/ymxdy/demo.java
@@ -0,0 +1,12 @@
+package cn.ymxdy;
+
+/**
+ * @author wf
+ * @date 2022/10/17 14:28
+ * @description
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello -world");
+    }
+}
