58389af6e6451a734ed38debffda4bbd431769c0
wangfeng <1290715192@qq.com>
2022-11-28 10:25:35 +0800

    git 回滚提交2

--- src/cn/ymxdy/demo.java
+++ src/cn/ymxdy/demo.java
@@ -1,5 +1,7 @@
 package cn.ymxdy;
 
+import java.util.HashSet;
+
 /**
  * @author wf
  * @date 2022/10/17 14:28
@@ -9,5 +11,21 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello -world");
         System.out.println("dev");
+        HashSet<String> set1 = new HashSet<>();
+        set1.add("1");
+        set1.add("2");
+        set1.add("3");
+        set1.add("4");
+
+        HashSet<String> set2 = new HashSet<>();
+        set2.add("1");
+        set2.add("7");
+        set2.add("3");
+        set2.add("9");
+
+        HashSet<String> set = new HashSet<>();
+        set.addAll(set1);
+        set.addAll(set2);
+        System.out.println(set);
     }
 }
